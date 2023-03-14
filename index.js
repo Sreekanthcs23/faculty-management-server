2fc43d103070c2d24e5f164b51affae2a47638e0
Sreekanthcs23 <sreekanthcs23@gmail.com>
2023-03-14 20:38:47 +0530

    education page added

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/education/data',(req,res) => {
+    const sqlSelect = "select * from faculty.education;";
+    db.query(sqlSelect,(err,result) => {
+        console.log(result);
+        res.send(result);
+    })
+})
+
 app.post('/education',(req,res) => {
     const degree = req.body.degree;
     const branch = req.body.branch;
@@ -26,7 +34,7 @@ app.post('/education',(req,res) => {
     const marksFloat = parseFloat(marks);
 
     const sqlInsert = "insert into faculty.education(userid,degree,branch,specialization,university,dateofacq,marks) values(?,?,?,?,?,?,?); ";
-    db.query(sqlInsert,[1,degree,branch,specialization,university,date,marksFloat],(err,res) => {
+    db.query(sqlInsert,[1,degree,branch,specialization,university,date,marksFloat],(err,result) => {
         console.log(err);
     })
 });
