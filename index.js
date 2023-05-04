4e29944a56d2c816d197a0b1489ae885ebec7d38
Sreekanthcs23 <sreekanthcs23@gmail.com>
2023-05-04 12:23:45 +0530

    04-05-23 s

--- index.js
+++ index.js
@@ -2,30 +2,126 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const jwt = require("jsonwebtoken");
+
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host:"facultydb.cslwxjjcqksg.ap-southeast-2.rds.amazonaws.com",
-    user:"admin",
-    password:process.env.AWS_PASSWORD,
+    host:"34.135.63.190",
+    user:"root",
+    password:process.env.GCP_PASSWD,
     database:"facultydb"
 }); 
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/education/data',(req,res) => {
-    const sqlSelect = "select * from faculty.education;";
+app.use(
+    session({
+      key: "userId",
+      secret: "subscribe",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        expires: 60 * 60 * 24,
+      },
+    })
+  );
+
+
+  app.get("/login", (req, res) => {
+    if (req.session.user) {
+      res.send({ loggedIn: true, user: req.session.user });
+    } else {
+      res.send({ loggedIn: false });
+    }
+  });
+  
+  app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log("inside login");
+    db.query(
+      "SELECT * FROM user WHERE username = ?;",
+      username,
+      (err, result) => {
+        if (err) {
+          res.send({ err: err });
+        }
+  
+        if (result.length > 0) {
+          bcrypt.compare(password, result[0].password, (error, response) => {
+            if (response) {
+
+                const id = result[0].userid;
+                const user = {userid:id};
+                console.log("id in login:"+user.userid);
+                const token = jwt.sign(user,"jwtsecret", {
+                    expiresIn: 300,
+                });
+                console.log("logged in");
+                req.session.user = result;
+                res.json({ auth: true, token: token, result: result});
+            } else {
+              res.json({auth:false,message: "wrong username / password"});
+              console.log("wrong password");
+            }
+          });
+        } else {
+          res.json({auth:false,message: "no user exists"});
+        }
+      }
+    );
+  });
+
+const verifyJWT = (req,res,next) => {
+    const token = req.headers["x-access-token"];
+    console.log("token:"+token);
+    if(!token) {
+        res.send("Yo we need a token, please give it to us next time!");
+    }else {
+        jwt.verify(token,"jwtsecret",(err,user) => {
+            if(err) {
+                res.json({auth:false,message:"U failed to authenticate"});
+            }else {
+                req.user = user;
+                console.log("userid:"+user.userid);
+                next();
+            }
+        });
+    }
+} 
+
+app.get("/isUserAuth",verifyJWT,(req,res)=> {
+    res.send("Yo u are authenticated congrats");
+});
+ 
+app.get('/education/select',(req,res) => {
+    const sqlSelect = "select * from education;";
     db.query(sqlSelect,(err,result) => {
-        console.log(result);
-        res.send(result);
+        console.log("fetched"+result);
+        res.json(result);
     })
 })
 
-app.post('/education',(req,res) => {
+app.post('/education/insert',(req,res) => {
     const degree = req.body.degree;
     const branch = req.body.branch;
     const specialization = req.body.specialization;
@@ -39,10 +135,33 @@ app.post('/education',(req,res) => {
     console.log(req.body);
     console.log(date);
 
-    const sqlInsert = "insert into faculty.education(userid,degree,branch,specialization,university,dateofacq,marks) values(?,?,?,?,?,?,?); ";
-    db.query(sqlInsert,[1,degree,branch,specialization,university,date,marksFloat],(err,result) => {
+    const sqlInsert = "insert into education(degree,branch,specialization,university,date_of_acq,marks,userid) values(?,?,?,?,?,?,?); ";
+    db.query(sqlInsert,[degree,branch,specialization,university,date,marksFloat,1],(err,result) => {
         console.log(err);
     }) 
+});
+
+app.get('/fundedproject/select',(req,res) => {
+    const sqlSelect = "select * from fundedProject;";
+    db.query(sqlSelect,(err,result) => {
+        console.log("fetched"+result);
+        res.json(result);
+    })
+});
+
+app.post('/fundedproject/insert',(req,res) => {
+    const name = req.body.name;
+    const agency = req.body.agency;
+    const amount = req.body.amount;
+    const period = req.body.period;
+    const dateFull = req.body.date;
+    const status = req.body.status;
+
+    const date = dateFull.toString().slice(0,10);
+    const sqlInsert = "insert into fundedProject(name,agency,amount,period,date_of_san,status,userid) values(?,?,?,?,?,?,?);";
+    db.query(sqlInsert,[name,agency,amount,period,date,status,1],(err,result) => {
+        console.log(err);
+    })
 });
 
 
