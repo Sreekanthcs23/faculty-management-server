592606bb714321003a4072bc9275aa99995fb5ec
Adarshms <msadarshatholi@gmail.com>
2023-06-19 12:14:37 +0530

    inserting and fetching full ok

--- src/routes/professional.routes.js
+++ src/routes/professional.routes.js
@@ -37,7 +37,7 @@ router.get("/select1", verifyJWT, (req,res) => {
 router.get("/select2", verifyJWT, (req,res) => {
   const sqlSelect = "select * from previous_experience where userid = " + req.user.userid + ";";
   db.query(sqlSelect,(err,result) => {
-      console.log("fetched"+result);
+      console.log("fetched "+result);
       console.log(result[0].from_date);
       res.json(result);
   })
@@ -46,13 +46,13 @@ router.get("/select2", verifyJWT, (req,res) => {
  // const upload = uploads({ dest: 'uploads/' }); 
   router.post("/insert1", verifyJWT, multer.single("appointmentOrder"), (req,res) => {
 
-    var publicUrl = '';  
+    var publicUrl1 = '';  
       try {
           if (req.file) {
             console.log("File found, trying to upload...");
             const blob = bucket.file(req.file.originalname);
-            publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-            console.log(publicUrl);
+            publicUrl1 = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+            console.log(publicUrl1);
             const blobStream = blob.createWriteStream();
       
             blobStream.on("finish", () => {
@@ -75,23 +75,28 @@ router.get("/select2", verifyJWT, (req,res) => {
       const promotionDate = promotionDate1.toString().slice(4,15);
       console.log(req.body);
 
-      const appointmentOrderUrl = publicUrl.split(" ").join("%20");
+      const appointmentOrderUrl = publicUrl1.split(" ").join("%20");
   
-      const sqlInsert = "insert into current_institution(userid,joining_date,joining_designation,date_of_problem_declaration,promotion_date,promotion_designation,appointment_order_link) values(?,?,?,?,?,?,?); ";
-      db.query(sqlInsert,[req.user.userid, joiningDate, joiningDesignation, dateofProblemDeclaration, promotionDate, promotionDesignation, appointmentOrderUrl],(err,result) => {
-          console.log(err);
-      }) 
+      const sqlUpdate = "UPDATE current_institution SET joining_date = ?, joining_designation = ?, date_of_problem_declaration = ?, promotion_date = ?, promotion_designation = ?, appointment_order_link = ? WHERE userid = ?";
+db.query(sqlUpdate, [joiningDate, joiningDesignation, dateofProblemDeclaration, promotionDate, promotionDesignation, appointmentOrderUrl, req.user.userid], (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Table updated successfully.");
+  }
+});
+ 
   });
 
    router.post("/insert1Pdf2", verifyJWT, multer.single("problemDeclaration"), (req,res) => {
 
-    var publicUrl = '';  
+    var publicUrl2 = '';  
       try {
           if (req.file) {
             console.log("File found, trying to upload...");
             const blob = bucket.file(req.file.originalname);
-            publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-            console.log(publicUrl);
+            publicUrl2 = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+            console.log(publicUrl2);
             const blobStream = blob.createWriteStream();
       
             blobStream.on("finish", () => {
@@ -102,22 +107,27 @@ router.get("/select2", verifyJWT, (req,res) => {
         } catch (error) {
           res.status(500).send(error);
       }
-      const problemDeclarationUrl = publicUrl.split(" ").join("%20");
-      const sqlInsert = "insert into current_institution(problem_declaration_link) values(?) where userid = "+ req.user.userid +";" ; 
-      db.query(sqlInsert,[problemDeclarationUrl],(err,result) => {
+      const problemDeclarationUrl = publicUrl2.split(" ").join("%20");
+      const sqlUpdate = "UPDATE current_institution SET problem_declaration_link = ? WHERE userid = ?";
+      db.query(sqlUpdate, [problemDeclarationUrl, req.user.userid], (err, result) => {
+        if (err) {
           console.log(err);
-      }) 
+        } else {
+          console.log("Table updated successfully.");
+        }
+      });
+
   });
 
   router.post("/insert1Pdf3", verifyJWT, multer.single("promotionOrder"), (req,res) => {
 
-    var publicUrl = '';  
+    var publicUrl3 = '';  
       try {
           if (req.file) {
             console.log("File found, trying to upload...");
             const blob = bucket.file(req.file.originalname);
-            publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-            console.log(publicUrl);
+            publicUrl3 = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+            console.log(publicUrl3);
             const blobStream = blob.createWriteStream();
       
             blobStream.on("finish", () => {
@@ -128,11 +138,16 @@ router.get("/select2", verifyJWT, (req,res) => {
         } catch (error) {
           res.status(500).send(error);
       }
-      const promotionOrderUrl = publicUrl.split(" ").join("%20");
-      const sqlInsert = "insert into current_institution(promotion_order_link) values(?) where userid = " + req.user.userid + ";";
-      db.query(sqlInsert,[promotionOrderUrl],(err,result) => {
+      const promotionOrderUrl = publicUrl3.split(" ").join("%20");
+      const sqlUpdate = "UPDATE current_institution SET promotion_order_link = ? WHERE userid = ?";
+      db.query(sqlUpdate, [promotionOrderUrl, req.user.userid], (err, result) => {
+        if (err) {
           console.log(err);
-      }) 
+        } else {
+          console.log("Table updated successfully.");
+        }
+      });
+      
   } );
 
   router.post("/insert2", verifyJWT, multer.single("experienceCertificate"),(req,res) => {
